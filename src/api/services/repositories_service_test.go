3912803e0466706efb8c2a999f7858bc8598108b
demby_abella <dmcchy@gmail.com>
2021-05-06 18:20:13 +0800

    Lesson 20. Testing Concurrent Models (Finished

--- src/api/services/repositories_service_test.go
+++ src/api/services/repositories_service_test.go
@@ -3,11 +3,13 @@ package services
 import (
 	"github.com/dembygenesis/go-rest-industry-standard/src/api/clients/restclient"
 	"github.com/dembygenesis/go-rest-industry-standard/src/api/domain/repositories"
+	"github.com/dembygenesis/go-rest-industry-standard/src/api/utils/errors"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 	"testing"
 )
 
@@ -65,4 +67,148 @@ func TestCreateRepoNoError(t *testing.T) {
 	assert.EqualValues(t, "", result.Name)
 	assert.EqualValues(t, "", result.Owner)
 
+}
+
+func TestCreateRepoConcurrentErrorFromGithub(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://api.github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusUnauthorized,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"status": 401,"message": "Requires authentication"}`)),
+		},
+		Err: nil,
+	})
+	request := repositories.CreateRepoRequest{Name: "testing"}
+
+	output := make(chan repositories.CreateRepositoriesResult)
+	service := reposService{}
+	go service.createRepoConcurrent(request, output)
+
+	result := <-output
+	assert.NotNil(t, result)
+	assert.Nil(t, result.Response)
+	assert.NotNil(t, result.Error)
+	assert.EqualValues(t, http.StatusUnauthorized, result.Error.Status())
+	assert.EqualValues(t, "Requires authentication", result.Error.Message())
+}
+
+func TestCreateRepoConcurrentNoError(t *testing.T) {
+	input := make(chan repositories.CreateRepositoriesResult)
+	output := make(chan repositories.CreateReposResponse)
+
+	var wg sync.WaitGroup
+
+	service := reposService{}
+	go service.handleRepoResults(&wg, input, output)
+
+	wg.Add(1)
+	go func() {
+		input <- repositories.CreateRepositoriesResult{
+			Error: errors.NewBadRequestError("invalid repository name"),
+		}
+	}()
+
+	wg.Wait()
+	close(input)
+
+	result := <-output
+
+	assert.NotNil(t, result)
+	assert.EqualValues(t, 0, result.StatusCode)
+	assert.EqualValues(t, 1, len(result.Results))
+	assert.NotNil(t, result.Results[0].Error)
+	assert.EqualValues(t, http.StatusBadRequest, result.Results[0].Error.Status())
+	assert.EqualValues(t, "invalid repository name", result.Results[0].Error.Message())
+}
+
+func TestCreateReposInvalidRequests(t *testing.T) {
+	requests := []repositories.CreateRepoRequest{
+		{},
+		{Name: "   "},
+	}
+
+	result, err := RepositoryService.CreateRepos(requests)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.EqualValues(t, 2, len(result.Results))
+	assert.EqualValues(t, http.StatusBadRequest, result.Results[0].Error.Status())
+	assert.EqualValues(t, http.StatusBadRequest, result.Results[1].Error.Status())
+	assert.Nil(t, result.Results[0].Response)
+	assert.Nil(t, result.Results[1].Response)
+	assert.EqualValues(t, "invalid repository name", result.Results[0].Error.Message())
+	assert.EqualValues(t, "invalid repository name", result.Results[1].Error.Message())
+}
+
+
+func TestCreateReposOneSuccessOneFail(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://api.github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusCreated,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"id": 123}`)),
+		},
+	})
+
+	requests := []repositories.CreateRepoRequest{
+		{},
+		{Name: "testing"},
+	}
+
+	result, err := RepositoryService.CreateRepos(requests)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.EqualValues(t, 2, len(result.Results))
+	assert.EqualValues(t, http.StatusPartialContent, result.StatusCode)
+
+	for _, result := range result.Results {
+		if result.Error != nil {
+			assert.EqualValues(t, http.StatusBadRequest, result.Error.Status())
+			assert.EqualValues(t, "invalid repository name", result.Error.Message())
+			continue
+		}
+
+		assert.EqualValues(t, 123, result.Response.Id)
+		assert.EqualValues(t, "", result.Response.Name)
+		assert.EqualValues(t, "", result.Response.Owner)
+	}
+}
+
+func TestCreateReposAllSuccess(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://api.github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusCreated,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"id": 123, "name": "testing", "owner": {"login": "demby"}`)),
+		},
+	})
+
+	requests := []repositories.CreateRepoRequest{
+		{Name: "testing"},
+		{Name: "testing"},
+	}
+
+	result, err := RepositoryService.CreateRepos(requests)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.EqualValues(t, http.StatusCreated, result.StatusCode)
+	assert.EqualValues(t, 2, len(result.Results))
+
+	assert.Nil(t, result.Results[0].Error)
+	assert.EqualValues(t, 123, result.Results[0].Response.Id)
+	assert.EqualValues(t, "testing", result.Results[0].Response.Name)
+	assert.EqualValues(t, "", result.Results[0].Response.Owner)
+
+	assert.Nil(t, result.Results[1].Error)
+	assert.EqualValues(t, 123, result.Results[1].Response.Id)
+	assert.EqualValues(t, "testing", result.Results[1].Response.Name)
+	assert.EqualValues(t, "", result.Results[1].Response.Owner)
 }
